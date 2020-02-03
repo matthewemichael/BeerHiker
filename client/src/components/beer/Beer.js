a096e4dd2115818df41685e3e5fcd27cc86435cd
James Conry <james.conry@comcast.net>
2020-02-03 13:18:08 -0600

    added other page files

--- client/src/components/beer/Beer.js
+++ client/src/components/beer/Beer.js
@@ -1,35 +1,214 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import API from "../../utils/API";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions"
+import NavBarIn from "../NavBarIn/NavBarIn";
 
 class Beer extends Component {
+
+  state = {
+    user: this.props.auth,
+    city: "",
+    state: "",
+    type: "",
+    name: ""
+  };
+
+  saveSearch = () => {
+    let searchData = [this.state.state, this.state.city, this.state.name, this.state.type];
+    API.saveSearch(searchData, this.state.user.user.id)
+  };
+
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
+  onSearchClick = e => {
+    e.preventDefault();
+    
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+
     render() {
       return (
-        <div style={{ height: "75vh" }} className="container valign-wrapper">
-          <div className="row">
-            <div className="col s12 center-align">
-              <h4>
+        <div className="background">
+          <NavBarIn />
+          <div className="container">
+            <br />
+            <div className="row center">
+              <h1>
                 {" "}
-                <span style={{ fontFamily: "monospace" }}>SEARCH FOR A BREWERY</span>
-              </h4>
-                <br />
-                <Link
-                to="/dashboard"
-                style={{
-                  width: "150px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                  marginTop: "1rem"
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Home
-              </Link>
+                <span>SEARCH FOR A BREWERY</span>
+              </h1>
+            </div>
+            <br />
+            <div className="columns is-centered">
+              <div className="column is-two-thirds">
+                <div className="field is-horizontal">
+                  <div className="field-label is-normal">
+                    <label className="label">City</label>
+                  </div>
+                  <div className="field-body">
+                    <div className="field">
+                      <input className="input" type="text" placeholder="City" name="city" onChange={this.handleInputChange}/>
+                      <p />
+                    </div>
+                  </div>
+                </div>
+              <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                  <label className="label">State</label>
+                </div>
+                <div className="field-body">
+                  <div className="field is-wide">
+                    <div className="control">
+                      <div className="select is-fullwidth">
+                        <select name="state" onChange={this.handleInputChange}>
+                          <option value />
+                          <option value="Alabama">Alabama</option>
+                          <option value="Alaska">Alaska</option>
+                          <option value="Arkansas">Arkansas</option>
+                          <option value="Arizona">Arizona</option>
+                          <option value="California">California</option>
+                          <option value="Colorado">Colorado</option>
+                          <option value="Connecticut">Connecticut</option>
+                          <option value="Delaware">Delaware</option>
+                          <option value="District_of_Columbia">District of Columbia</option>
+                          <option value="Florida">Florida</option>
+                          <option value="Georgia">Georgia</option>
+                          <option value="Hawaii">Hawaii</option>
+                          <option value="Idaho">Idaho</option>
+                          <option value="Illinois">Illinois</option>
+                          <option value="Indiana">Indiana</option>
+                          <option value="Iowa">Iowa</option>
+                          <option value="Kansas">Kansas</option>
+                          <option value="Kentucky">Kentucky</option>
+                          <option value="Louisiana">Louisiana</option>
+                          <option value="Maine">Maine</option>
+                          <option value="Maryland">Maryland</option>
+                          <option value="Massachusetts">Massachusetts</option>
+                          <option value="Michigan">Michigan</option>
+                          <option value="Minnesota">Minnesota</option>
+                          <option value="Mississippi">Mississippi</option>
+                          <option value="Missouri">Missouri</option>
+                          <option value="Montana">Montana</option>
+                          <option value="Nebraska">Nebraska</option>
+                          <option value="Nevada">Nevada</option>
+                          <option value="New_Hampshire">New Hampshire</option>
+                          <option value="New_Jersey">New Jersey</option>
+                          <option value="New_Mexico">New Mexico</option>
+                          <option value="New_York">New York</option>
+                          <option value="North_Carolina">North Carolina</option>
+                          <option value="North_Dakota">North Dakota</option>
+                          <option value="Ohio">Ohio</option>
+                          <option value="Oklahoma">Oklahoma</option>
+                          <option value="Oregon">Oregon</option>
+                          <option value="Pennsylvania">Pennsylvania</option>
+                          <option value="Puerto_Rico">Puerto Rico</option>
+                          <option value="Rhode_Island">Rhode Island</option>
+                          <option value="Sout_Carolina">South Carolina</option>
+                          <option value="South_Dakota">South Dakota</option>
+                          <option value="Tennessee">Tennessee</option>
+                          <option value="Texas">Texas</option>
+                          <option value="Utah">Utah</option>
+                          <option value="Vermont">Vermont</option>
+                          <option value="Virginia">Virginia</option>
+                          <option value="Washington">Washington</option>
+                          <option value="West_Virginia">West Virginia</option>
+                          <option value="Wisconsin">Wisconsin</option>
+                          <option value="Wyoming">Wyoming</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                  <label className="label">Brewery Name</label>
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <input className="input" type="text" placeholder="Optional" name="name" onChange={this.handleInputChange}/>
+                    <p />
+                  </div>
+                </div>
+              </div>
+              <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                  <label className="label">Brewery Type</label>
+                </div>
+                <div className="field-body">
+                  <div className="field is-wide">
+                    <div className="control">
+                      <div className="select is-fullwidth">
+                        <select name="type" onChange={this.handleInputChange}>
+                          <option value>Optional</option>
+                          <option value="bar">Bar</option>
+                          <option value="micro">Micro Brewery</option>
+                          <option value="regional">Regional Brewery</option>
+                          <option value="brewpub">Brewpub</option>
+                          <option value="large">Large Brewery</option>
+                          <option value="planning">In planning</option>
+                          <option value="contract">Contract Brewery</option>
+                          <option value="proprietor">Proprietor</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="field is-horizontal">
+                <div className="field-label">
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <Link
+                        to="/results"
+                        style={{
+                          width: "150px",
+                          borderRadius: "3px",
+                          letterSpacing: "1.5px",
+                          marginTop: "1rem"
+                        }}
+                        onClick = {this.saveSearch}
+                        className="btn btn-large waves-effect waves-light hoverable orange accent-3"
+                      >
+                        Submit
+                      </Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        </div>
+      
       );
     }
   }
+
+  Beer.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+  };
   
-  export default Beer;
+  const mapStateToProps = state => ({
+    auth: state.auth
+  });
+  
+  export default connect(
+    mapStateToProps,
+    { logoutUser }
+  )(Beer);
   
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,17 +3,57 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import { Link } from "react-router-dom";
+import NavBarIn from "../NavBarIn/NavBarIn"
+import API from "../../utils/API";
+import NavBarDash from "../NavBarDash/NavBarDash";
 
 class Dashboard extends Component {
-  onLogoutClick = e => {
-    e.preventDefault();
-    this.props.logoutUser();
+
+  state = {
+    user: this.props.auth,
+    searchData: []
   };
+
+  componentDidMount() {
+    API.getSearchData(this.state.user.user.id)
+      .then(res =>
+        this.setUserState(res.data),
+      )
+      .catch(err => console.log(err));
+   }
+
+   setUserState = (data) => {
+    this.setState({searchData: data});
+  }
+
+
+  onSearchClick = e => {
+    e.preventDefault();
+    
+  }
 
   render() {
     const { user } = this.props.auth;
 
     return (
+      <div class="background">
+        <NavBarDash />
+        <section class="hero col s12 center-align">
+        <div class="hero-head">
+          <div class="container">
+          <section class="hero is-large has-sm-img"> 
+        <div class="hero-body"> 
+          <div class="container has-text-centered"> 
+          <div className="container valign-wrapper">
+          </div>
+            </div> 
+          </div> 
+        </section>
+          </div>
+        </div>
+      </section>
+      <br/>
+      <div className="container registerForm">
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="landing-copy col s12 center-align">
@@ -24,18 +64,7 @@ class Dashboard extends Component {
                 <span style={{ fontFamily: "monospace" }}>BEER HIKER</span>
               </p>
             </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
+          
             <Link
                 to="/beer"
                 style={{
@@ -44,12 +73,65 @@ class Dashboard extends Component {
                   letterSpacing: "1.5px",
                   marginTop: "1rem"
                 }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                className="btn btn-large waves-effect waves-light hoverable orange accent-3"
               >
                 Search
               </Link>
           </div>
         </div>
+        <div className="row">
+            
+            {this.state.searchData.search ? (
+                this.state.searchData.search.map(brew => (
+                    <p>
+                    Last Searches: {brew || "Not Used"}
+                  </p>
+                )
+                )
+
+                // <p>Last Search: {this.state.searchData.search[0]}</p>
+                // <p>Last Mapped: {this.state.searchData.mapBreweries[0].name}</p>
+                
+              ) : (
+                <h3>No Search Data to Display</h3>
+              )}
+          </div>
+          <div className="row">
+            
+            {this.state.searchData.mapBreweries ? (
+                this.state.searchData.mapBreweries.map(brew => (
+                    <p>
+                    Last Mapped: {brew.name}
+                  </p>
+                )
+                )
+
+                // <p>Last Search: {this.state.searchData.search[0]}</p>
+                // <p>Last Mapped: {this.state.searchData.mapBreweries[0].name}</p>
+                
+              ) : (
+                <h3>No Search Data to Display</h3>
+              )}
+          </div>
+          <div className="row">
+            
+            {this.state.searchData.savedBreweries ? (
+                this.state.searchData.savedBreweries.map(brew => (
+                    <p>
+                    Last Saved: {brew.name}
+                  </p>
+                )
+                )
+
+                // <p>Last Search: {this.state.searchData.search[0]}</p>
+                // <p>Last Mapped: {this.state.searchData.mapBreweries[0].name}</p>
+                
+              ) : (
+                <h3>No Search Data to Display</h3>
+              )}
+          </div>
+      </div>
+      </div>
       </div>
     );
   }
@@ -68,3 +150,5 @@ export default connect(
   mapStateToProps,
   { logoutUser }
 )(Dashboard);
+
+
